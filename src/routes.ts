dac883016af65b8b13bafadc411f727c431fdd8f
Michał <michal@garapich.pl>
2019-03-28 19:53:45 +0100

    report version

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 import { routes as vatsim } from './vatsim';
 
+// tslint:disable-next-line:no-var-requires
+const { version } = require('../package.json');
+
 export class Routes {
   public routes(app: express.Application): void {
+    app.route('/').get((req, res) => res.status(200).send({ version }));
     app.use('/vatsim', vatsim);
   }
 }
