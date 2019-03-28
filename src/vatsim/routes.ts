9b0af8fb79170381313a5b182a249acc39dc2b78
Michał <michal@garapich.pl>
2019-03-28 19:53:58 +0100

    add vatsim pilots & atc routes

--- src/vatsim/routes.ts
+++ src/vatsim/routes.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import { isAtc } from './models/atc';
+import { isPilot } from './models/pilot';
 import vatsimTracker from './vatsim-tracker';
 
 const router = Router();
@@ -10,5 +12,13 @@ router
 router
   .route('/data')
   .get((req, res) => res.status(200).send(vatsimTracker.data));
+
+router
+  .route('/data/pilots')
+  .get((req, res) => res.status(200).send(vatsimTracker.data.clients.filter(client => isPilot(client))));
+
+router
+  .route('/data/atc')
+  .get((req, res) => res.status(200).send(vatsimTracker.data.clients.filter(client => isAtc(client))));
 
 export default router;
