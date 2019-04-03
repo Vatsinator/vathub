1a1355d7077715fdec4e38e28bfdbdd8f2995ecc
Michał Garapich <michal@garapich.pl>
2019-04-03 03:02:33 +0200

    handle process.env.PORT

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import app from './app';
 import logger from './logger';
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   logger.info(`running on port ${PORT}`);
