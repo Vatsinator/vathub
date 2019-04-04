6906e102583a5c979d9baa17140103489010783a
Michał Garapich <michal@garapich.pl>
2019-04-05 01:07:20 +0200

    feat: Fir model update

--- src/firs/models/fir.ts
+++ src/firs/models/fir.ts
@@ -1,8 +1,10 @@
+import { LatLng } from '../../vatsim/models';
+
 export interface Fir {
   icao: string;
   name: string;
   country: string;
   oceanic: boolean;
-  border: Array<{ latitude: number; longitude: number }> | Array<Array<{ latitude: number; longitude: number }>>;
-  labelPosition: { latitude: number; longitude: number };
+  border: LatLng[][];
+  labelPosition: LatLng;
 }
