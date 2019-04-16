ace4e4a8bfcb57d800aca1c049e9984abf15e1b1
Michał <michal@garapich.pl>
2019-04-16 09:51:39 +0200

    feat: handle GET with multiple ICAO codes

--- src/firs/routes.ts
+++ src/firs/routes.ts
@@ -4,19 +4,46 @@ import { uirList } from './uir-list';
 
 const router = Router();
 
+function findByIcao(icao: string) {
+  return firList.find(f => f.icao === icao) || uirList.find(u => u.icao === icao);
+}
+
 router
   .route('/')
-  .get((req, res) => res.status(200).send(firList));
+  .get((req, res) => {
+    if (req.query.icao) {
+      const icaos = req.query.icao.split(',');
+      const firs = [];
+      const errors = [];
+
+      for (const icao of icaos) {
+        const fir = findByIcao(icao);
+        if (!fir) {
+          errors.push(icao);
+        } else {
+          firs.push(fir);
+        }
+      }
+
+      if (errors.length > 0) {
+        res.status(404).send({ message: `FIR(s) ${errors.join(', ')} don't exist` });
+      } else {
+        res.status(200).send(firs);
+      }
+    } else {
+      res.status(200).send(firList);
+    }
+  });
 
 router
   .route('/:firIcao')
   .get((req, res) => {
     const icao = req.params.firIcao;
-    const fir = firList.find(f => f.icao === icao) || uirList.find(u => u.icao === icao);
+    const fir = findByIcao(icao);
     if (fir) {
       res.status(200).send(fir);
     } else {
-      res.status(404).send({ message: `FIR ${icao} doesn\'t exist` });
+      res.status(404).send({ message: `FIR ${icao} doesn't exist` });
     }
   });
 
