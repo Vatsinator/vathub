8ee70e1503a7d3d426bdee643f3c35314cd8e607
Michał <michal@garapich.pl>
2019-07-29 11:04:04 +0200

    fix geolib renamed function

--- src/vatsim/discover-flight-phase.ts
+++ src/vatsim/discover-flight-phase.ts
@@ -1,4 +1,4 @@
-import { isPointInCircle } from 'geolib';
+import { isPointWithinRadius } from 'geolib';
 import { findAirportByIcao } from '../airports';
 import { FlightPhase, Pilot } from './models/pilot';
 import { PilotIsInAirportRange } from './pilot-is-in-airport-range';
@@ -7,7 +7,7 @@ export function discoverFlightPhase(pilot: Pilot): FlightPhase {
   if (pilot.groundSpeed < 50) {
     const dep = findAirportByIcao(pilot.from);
     if (dep) {
-      if (isPointInCircle(
+      if (isPointWithinRadius(
         { latitude: pilot.position[0], longitude: pilot.position[1] },
         { latitude: dep.position[0], longitude: dep.position[1] }, PilotIsInAirportRange)) {
         return 'departing';
@@ -16,7 +16,7 @@ export function discoverFlightPhase(pilot: Pilot): FlightPhase {
 
     const dest = findAirportByIcao(pilot.to);
     if (dest) {
-      if (isPointInCircle(
+      if (isPointWithinRadius(
         { latitude: pilot.position[0], longitude: pilot.position[1] },
         { latitude: dest.position[0], longitude: dest.position[1] }, PilotIsInAirportRange)) {
         return 'arrived';
